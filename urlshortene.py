cf2892f43bcd45674a8c6b04050edeb1fceb4469
Govardhan Katta <83834179+KattaGovardhan@users.noreply.github.com>
2023-05-31 20:37:18 +0530

    Add files via upload

---
+++ urlshortene.py
@@ -0,0 +1,9 @@
+#install pyshorteners using command pip install pyshorteners
+import pyshorteners
+
+url=input("Enter the URL: ")
+def shortenurl(url):
+    s = pyshorteners.Shortener()
+    print(s.tinyurl.short(url))
+
+shortenurl(url)
